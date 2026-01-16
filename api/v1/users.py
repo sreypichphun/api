f3c18d43439d1ff43aee4dfca119e388a16606b3
sreypichphun <sreypichphun83@gmail.com>
2026-01-16 11:29:58 +0700

    Update router

--- api/v1/users.py
+++ api/v1/users.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from db.session import get_db
 from schemas.user import UserCreate, UserResponse
 from services.user_service import create_user
+from models.user import User
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -16,3 +17,11 @@ def create_user_endpoint(
     db: Session = Depends(get_db)
 ):
     return create_user(db, payload.email, payload.name)
+
+@router.get(
+    "/",
+    response_model=list[UserResponse]
+)
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
+
